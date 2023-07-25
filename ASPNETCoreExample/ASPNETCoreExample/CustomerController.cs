dac60f9bfce0e19d0906acb75cf7a2f01b09a5cf
Wkachi <123011868+Wkachi@users.noreply.github.com>
2023-07-25 16:58:37 +0000

    Add files via upload

---
+++ ASPNETCoreExample/ASPNETCoreExample/CustomerController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace ASPNETCoreExample
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<CustomerController> _logger;
+
+        public CustomerController(ILogger<CustomerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetCustomers")]
+
+        
+            public IEnumerable<WeatherForecast> Get()
+            {
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    //Date = DateTime.Now.AddDays(index),
+                    //TemperatureC = Random.Shared.Next(-20, 55),
+                    //Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+            }
+
+        }
+    
+}
